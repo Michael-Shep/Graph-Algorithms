b805251da48d8e83bb41630965cd825b6659f6e2
Michael-Shep <mgShepherd1504@gmail.com>
2021-09-08 14:53:15 +0100

    Added a list of connctions for each node displayed in the the information panel

--- graph-algorithms/src/components/InformationPanel/InformationPanel.tsx
+++ graph-algorithms/src/components/InformationPanel/InformationPanel.tsx
@@ -6,6 +6,7 @@ import store, { NodeState } from '../../helpers/ReduxStore';
 
 interface InformationPanelProps {
   nodes: NodeData[];
+  connectionIndexes: number[][];
   selectedNodeIndex: number;
 }
 
@@ -27,6 +28,34 @@ const InformationPanel = (props: InformationPanelProps) => {
     }
   };
 
+  const doesConnectionInvolveSelectedNode = (
+    nodeIndexes: number[]
+  ): boolean => {
+    return nodeIndexes.includes(props.selectedNodeIndex);
+  };
+
+  const listConnectionsForNode = () => {
+    const nodeConnections = props.connectionIndexes.filter(
+      doesConnectionInvolveSelectedNode
+    );
+
+    if (nodeConnections.length === 0) {
+      return <p> This node currently has no connections </p>;
+    }
+
+    return nodeConnections.map((connection, index) => {
+      const otherNodeIndex =
+        connection[0] === props.selectedNodeIndex
+          ? connection[1]
+          : connection[0];
+      return (
+        <p key={index} className="leftAligned">
+          {index + 1}: Node with value {props.nodes[otherNodeIndex].value}
+        </p>
+      );
+    });
+  };
+
   const getBody = () => {
     if (props.selectedNodeIndex === -1) {
       return <div> Select a node to view and modify its information here</div>;
@@ -42,6 +71,8 @@ const InformationPanel = (props: InformationPanelProps) => {
             value={props.nodes[props.selectedNodeIndex].value}
             onChange={handleValueChange}
           />
+          <p className="formSubheading">Connections:</p>
+          {listConnectionsForNode()}
         </form>
       </div>
     );
@@ -57,6 +88,7 @@ const InformationPanel = (props: InformationPanelProps) => {
 
 const mapStateToProps = (state: NodeState) => ({
   nodes: state.nodes,
+  connectionIndexes: state.connectionIndexes,
   selectedNodeIndex: state.selectedNodeIndex,
 });
 
